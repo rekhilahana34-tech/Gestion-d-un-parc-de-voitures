fec551d367bd3147faace6ee8b85c88e686446a0
rekhilahana34-tech <rekhilahana34@gmail.com>
2026-03-12 12:28:25 -0400

    les méthodes

--- 1.py
+++ 1.py
@@ -2,3 +2,6 @@ class Voiture:
     def __init__(self, marque, modele):
         self.marque = marque
         self.modele = modele
+
+    def affiche(self):
+        print(f"voiture {self.marque} {self.modele}")
