e6f52a77657e9ba0340654e0a8ded2753fdbb17d
rekhilahana34-tech <rekhilahana34@gmail.com>
2026-03-12 12:30:07 -0400

    classe Parc

--- 1.py
+++ 1.py
@@ -5,3 +5,9 @@ class Voiture:
 
     def affiche(self):
         print(f"voiture {self.marque} {self.modele}")
+class Parc:
+    def __init__(self,id,adresse,capacite):
+        self.id =id
+        self.adresse = adresse
+        self.capacite = capacite
+        self.listeVoitures = []
